04feae847c4b3f49c91ea4b8104eae3f4d2a209f
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2021-09-18 21:09:59 +0800

    added multi site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+import Cookies from 'universal-cookie';
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    const cookies = new Cookies();
+    cookies.set('version', 'oldApp', { path: '/' });
+    // window.location.reload();
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Version - {process.env.REACT_APP_VERSION}</h2>
+      <button onClick={handleClick}>
+        Navigate to First App
+      </button>
     </div>
   );
 }
